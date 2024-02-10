b694dddd038c5b01d413d5dc5211fbaaa1dade89
Tutul Kabir <tutulkabir@gmail.com>
2024-02-10 11:51:17 +0530

    font family and customize font family

---
+++ 4.Font Family in Tailwind CSS/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      display: ["Oswald"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
